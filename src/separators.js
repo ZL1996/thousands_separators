7fd5578806a29357a209e02bd816c4d19d7575e9
ZL1996 <649349296@qq.com>
2016-05-26 12:03:31 +0800

    Add files via upload

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,49 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+    var a = num.toString();
+    var b = split_dot(a);
+    var result = split_comma(b);
+    return result;
+}
+
+function split_dot(a) {
+    var a;
+    var str = [];
+
+    a = a.indexOf('.');
+    if (a === -1) {
+        str.push(a);
+    }
+    else {
+        str=a.split('.');
+    }
+
+    return str;
+}
+
+function split_comma(b) {
+    var result;
+
+    if (b[0].length <= 3) {
+        result = b[0];
+    }
+    else {
+        var pos = b[0].length % 3;
+        result = b[0].slice(0, pos) + ',';
+        while (b[0].length - pos > 3) {
+            result += b[0].slice(pos, pos + 3) + ',';
+            pos = pos + 3;
+        }
+        result += b[0].slice(pos);
+    }
+
+    if (b.length > 1) {
+        result += '.';
+        result += str[1];
+    }
+
+    return result;
 }
 
 module.exports = thousands_separators;
