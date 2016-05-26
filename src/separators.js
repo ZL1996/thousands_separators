6b5af035eb5d1f34c6f1a6c026a7cd18e47702e5
ZL1996 <649349296@qq.com>
2016-05-26 14:38:12 +0800

    Add files via upload

--- src/separators.js
+++ src/separators.js
@@ -1,49 +1,55 @@
 'use strict';
 
 function thousands_separators(num) {
-    var a = num.toString();
-    var b = split_dot(a);
-    var result = split_comma(b);
-    return result;
+    var newString = toString(num);
+    var newSegmentation = segmentation(newString);
+    var newIntegerPart = getIntegerPart(newSegmentation);
+    return newIntegerPart;
 }
 
-function split_dot(a) {
-    var a;
+function toString(num) {
+    var newString = num.toString();
+
+    return newString;
+}
+
+function segmentation(newString) {
+    var judge;
     var str = [];
 
-    a = a.indexOf('.');
-    if (a === -1) {
-        str.push(a);
+    judge = newString.indexOf(".");
+    if (judge === -1) {
+        str.push(newString);
     }
     else {
-        str=a.split('.');
+        str=newString.split(".");
     }
 
     return str;
 }
 
-function split_comma(b) {
-    var result;
+function getIntegerPart(str) {
+    var newInteger;
 
-    if (b[0].length <= 3) {
-        result = b[0];
+    if (str[0].length <= 3) {
+        newInteger = str[0];
     }
     else {
-        var pos = b[0].length % 3;
-        result = b[0].slice(0, pos) + ',';
-        while (b[0].length - pos > 3) {
-            result += b[0].slice(pos, pos + 3) + ',';
-            pos = pos + 3;
+        var position = str[0].length % 3;
+        newInteger = str[0].slice(0, position) + ',';
+        while (str[0].length - position > 3) {
+            newInteger += str[0].slice(position, position + 3) + ',';
+            position = position + 3;
         }
-        result += b[0].slice(pos);
+        newInteger += str[0].slice(position);
     }
 
-    if (b.length > 1) {
-        result += '.';
-        result += str[1];
+    if (str.length > 1) {
+        newInteger += '.';
+        newInteger += str[1];
     }
 
-    return result;
+    return newInteger;
 }
 
 module.exports = thousands_separators;
